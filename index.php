bedb6e4c6736f598748f0fdd100fb04a934d1b71
Nononoya <101647801+Nononoya@users.noreply.github.com>
2022-07-13 19:33:00 +0200

    First Commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Nononoya</title>
+</head>
+<body>
+<a href="nononoya.github.io/ani-find">Ani-Find</a>
+</body>
+</html>
